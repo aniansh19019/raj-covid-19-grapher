41d5dcf08b23207a278960788d28354d7865eb5f
Aniansh <aniansh@yahoo.com>
2020-04-20 01:40:41 +0530

    Testing env variables with email bug reporting+1

--- config.py
+++ config.py
@@ -4,11 +4,11 @@ class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
     #Mail creds
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+	MAIL_SERVER = os.environ.get('MAIL_SERVER')
+	MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+	MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 	ADMINS = ['aniansh@yahoo.com']
 	# MAIL_SERVER='smtp.googlemail.com'
 	# MAIL_PORT=587
