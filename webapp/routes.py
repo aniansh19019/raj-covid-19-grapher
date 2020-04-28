6b02cf16c942985172e4ef547dc979caf28e101e
Aniansh <aniansh@yahoo.com>
2020-04-29 04:01:54 +0530

    Debugging

--- webapp/routes.py
+++ webapp/routes.py
@@ -78,13 +78,11 @@ def poll_func(home=False):
 
 		user_agent_str=http_headers['User-Agent']
 		user_agent_obj=parse(user_agent_str)
-		try:
-			h=Headers(ip=ip, country=country, user_agent=user_agent_str, fancy_user_agent=str(user_agent_obj))
-			db.session.add(h)
-			db.session.commit()
-			print("pushed")
-		except:
-			print("Database Error! Could Not Push!")
+		h=Headers(ip=ip, country=country, user_agent=user_agent_str, fancy_user_agent=str(user_agent_obj))
+		db.session.add(h)
+		db.session.commit()
+		print("pushed")
+		# print("Database Error! Could Not Push!")
 	else:
 		pass
 
